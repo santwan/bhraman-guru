d05551d0b24726abc31e7dd954bdb21927b2e239
santwan <santwanpathak1@gmail.com>
2025-04-17 11:38:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div className='"min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors duration-300"'>
     <h1>Hello</h1>
-    </>
+    </div>
   )
 }
 
