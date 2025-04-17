66457a10b0ce5ed9ee4f8c0c464273a1948926dd
santwan <santwanpathak1@gmail.com>
2025-04-17 11:09:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import CreateTrip from './create-trip/index.jsx'
 
 const router=createBrowserRouter([
   {
     path:'/',
     element:<App/>,
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
   }
 ])
 
