1f0cd42768b06689b8456667b178720fcee4aa77
santwan <santwanpathak1@gmail.com>
2025-04-17 23:45:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CreateTrip from './pages/create-trip/CreateTrip.jsx'
 import Navbar from './components/global/Navbar.jsx'
+import { Toaster } from "@/components/ui/sonner.jsx"
 
 const router=createBrowserRouter([
   {
@@ -20,6 +21,7 @@ const router=createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Navbar/>
+    <Toaster/>
     <RouterProvider router={router} />
   </StrictMode>,
 )
