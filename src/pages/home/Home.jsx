ff16fae4334de6db267596fef19b522f53a290a9
santwan <santwanpathak1@gmail.com>
2025-04-17 13:13:12 +0530

    Create Home.jsx

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import { div } from "framer-motion/client";
+import React from "react";
+import HeroSection from "../../components/HeroSection.jsx";
+
+
+const Home = () => {
+    return(
+        <div className="h-[200vh] pt-34 bg-amber-300">
+            <HeroSection/>
+        </div>
+    )
+}
+
+
+export default Home
