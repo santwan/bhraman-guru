eba2b92ff1974f6464aa8b43aa2bcd81618383d6
santwan <santwanpathak1@gmail.com>
2025-04-17 13:13:07 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import CreateTrip from './create-trip/index.jsx'
+import CreateTrip from './pages/create-trip/CreateTrip.jsx'
 import Navbar from './components/global/Navbar.jsx'
 
 const router=createBrowserRouter([
