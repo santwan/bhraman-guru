016a50860e29892f2a5a42bf5c0b30b80eceb667
santwan <santwanpathak1@gmail.com>
2025-04-17 11:38:24 +0530

    Update HeroSection.jsx

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,13 @@
+import { div } from "framer-motion/client";
+import React from "react";
+
+
+const HeroSection = () => {
+    return (
+        <div>
+            <h1>This is our Hero Section</h1>
+        </div>
+    )
+}
+
+export default HeroSection
