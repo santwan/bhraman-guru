709f469545a68d3e48cfc9ff70cdd797b37f120e
santwan <santwanpathak1@gmail.com>
2025-04-17 11:09:33 +0530

    Update index.jsx

---
+++ src/create-trip/index.jsx
@@ -0,0 +1,13 @@
+import { div } from "framer-motion/client";
+import React from "react";
+
+
+const CreateTrip = () => {
+    return (
+        <div>
+            <h1>Create Trip Section</h1>
+        </div>
+    )
+}
+
+export default CreateTrip
