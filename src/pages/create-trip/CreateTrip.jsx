f3eea01eb21567f55e9ef5fbb1610b2dff94034f
santwan <santwanpathak1@gmail.com>
2025-04-17 20:49:50 +0530

    Update CreateTrip.jsx

--- src/pages/create-trip/CreateTrip.jsx
+++ src/pages/create-trip/CreateTrip.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
 import Input from '../../components/Input.jsx';
+import { SelectBudgetOptions, SelectTravelList } from '../../constants/option.jsx';
 
 function CreateTrip() {
     const [place,setPlace]=useState();
@@ -38,6 +39,59 @@ function CreateTrip() {
                     onChange={(e)=>handleInputChange('noOfDays', e.target.value)} 
                 />
             </div>
+
+
+            <div className='mt-10'>
+            <h2 className='font-semibold mb-0'>What is Your Budget?</h2>
+            <h3 className='font-normal mb-2'>The Budget is exclusively allocated for activities and dining purpose</h3>
+            <div className='grid grid-cols-3 gap-3'>
+            {SelectBudgetOptions.map((item, index) => {
+                const Icon = item.icon;
+                return (
+                    <div
+                    key={index}
+                    className="p-2 border cursor-pointer rounded-lg hover:shadow-lg"
+                    >
+                    <Icon className="w-8 h-8 text-red-600" />
+                    <h2 className="font-medium">{item.title}</h2>
+                    <h2 className="text-xs text-gray-500">{item.desc}</h2>
+                    </div>
+                );
+                })}
+            </div>
+
+            <div className='mt-10'>
+                <h2 className='font-semibold mb-2'>
+                    Who do you plan on traveling with on your next adventure?
+                </h2>
+                <div className='grid grid-cols-3 gap-2'>
+                    {SelectTravelList.map((item, index) => {
+                    const Icon = item.icon;
+                    return (
+                        <div
+                        key={index}
+                        onClick={() => handleInputChange('traveler', item.people)}
+                        className=
+                            "p-2 border cursor-pointer rounded-lg hover:shadow-lg transition-all"
+                        >
+                        <Icon className='w-8 h-8 text-indigo-600 mb-1' />
+                        <h2 className='font-medium'>{item.title}</h2>
+                        <h2 className='text-xs text-gray-500'>{item.desc}</h2>
+                        <h2 className=' text-gray-900'>{item.people}</h2>
+                        </div>
+                    );
+                    })}
+                </div>
+            </div>
+
+
+            <div className='mt-10 text-right'>
+                <Button onClick={onGenerateTrip} disabled={loading}>
+                    {loading?
+                    <AiOutlineLoading3Quarters className='h-10 w-10 animate-spin'/>: 'Generate Trip'}
+                </Button>
+            </div>
+        </div>
         </div>
     </div>
   )
