72ad326d582b0ff877d56e800ccbfd270e6a9fcc
santwan <santwanpathak1@gmail.com>
2025-04-18 11:57:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,40 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import CreateTrip from './pages/create-trip/CreateTrip.jsx'
-import Navbar from './components/global/Navbar.jsx'
+import Layout from './components/global/Layout.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Toaster } from "@/components/ui/sonner.jsx"
 import { ClerkProvider } from '@clerk/clerk-react'
 
-// Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-// console.log(PUBLISHABLE_KEY)
 
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Publishable Key")
 }
 
-
-const router=createBrowserRouter([
+const router = createBrowserRouter([
   {
-    path:'/',
-    element:<App/>,
-  },
-  {
-    path:'/create-trip',
-    element:<CreateTrip/>
+    path: '/',
+    element: <Layout />, // Shared layout for all child routes
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+      {
+        path: '/create-trip',
+        element: <CreateTrip />
+      }
+    ]
   }
 ])
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <Navbar/>
-      <Toaster/>
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Toaster />
       <RouterProvider router={router} />
-      </ClerkProvider>
-    </StrictMode>
-  
+    </ClerkProvider>
+  </StrictMode>
 )
