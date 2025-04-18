f1d2d10457eeeb6a5a58d6f88fb504303c34dd5c
santwan <santwanpathak1@gmail.com>
2025-04-18 20:13:12 +0530

    Update MyTrips.jsx
    Just testing that this page is rendering propely

---
+++ src/pages/my-trips/MyTrips.jsx
@@ -0,0 +1,13 @@
+import { h1 } from "framer-motion/client";
+import React from "react";
+
+
+const MyTrips = () => {
+
+
+    return(
+        <h1 className="pt-30">This is our My trip section</h1>
+    )
+}
+
+export default MyTrips
