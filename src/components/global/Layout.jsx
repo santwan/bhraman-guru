0f5722048631edd60dcfea39643d2db5934ef5cb
santwan <santwanpathak1@gmail.com>
2025-04-20 12:25:43 +0530

    Update Layout.jsx

--- src/components/global/Layout.jsx
+++ src/components/global/Layout.jsx
@@ -4,10 +4,10 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <>
+    <div className="min-h-screen bg-white text-black dark:bg-[#0d0d0d] dark:text-white transition-colors duration-300 ease-in-out">
       <Navbar />
       <Outlet /> {/* This is where child routes will be rendered */}
-    </>
+    </div>
   )
 }
 
