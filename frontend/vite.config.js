1aaed7eb8f18b8b5e4611d5f024e1ca1dddd4225
santwan <santwanpathak1@gmail.com>
2025-04-20 19:08:38 +0530

    Create vite.config.js

---
+++ frontend/vite.config.js
@@ -0,0 +1,24 @@
+// vite.config.js
+import path from "path"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    // this block makes Vite forward `/api/**` to your backend
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+})
