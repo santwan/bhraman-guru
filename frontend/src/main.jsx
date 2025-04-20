f47b02e6a8cb3300dc913425a942bd5d735cf8be
santwan <santwanpathak1@gmail.com>
2025-04-20 19:10:01 +0530

    Create main.jsx

---
+++ frontend/src/main.jsx
@@ -0,0 +1,51 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import CreateTrip from './pages/create-trip/CreateTrip.jsx'
+import Layout from './components/global/Layout.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Toaster } from "@/components/ui/sonner.jsx"
+import { ClerkProvider } from '@clerk/clerk-react'
+import MyTrips from './pages/my-trips/MyTrips.jsx'
+import TripHistory from './pages/my-trips/trip-history/TripHistory.jsx'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key")
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />, // Shared layout for all child routes
+    children: [
+      {
+        path: '/',
+        element: <App />
+      },
+      {
+        path: '/create-trip',
+        element: <CreateTrip />
+      },
+      {
+        path: '/my-trips',
+        element: <MyTrips />
+      },
+      {
+        path: '/trip-history',
+        element: <TripHistory/>
+      }
+    ]
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Toaster />
+      <RouterProvider router={router} />
+    </ClerkProvider>
+  </StrictMode>
+)
