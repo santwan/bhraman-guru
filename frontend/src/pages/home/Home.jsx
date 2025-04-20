2bc3a081ce273353416901639d33757375062591
santwan <santwanpathak1@gmail.com>
2025-04-20 19:08:54 +0530

    Create Home.jsx

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import { div } from "framer-motion/client";
+import React from "react";
+import HeroSection from "../../components/HeroSection.jsx";
+import TestimonialSection from "../../components/TestimonialSection.jsx";
+import BenefitSection from "../../components/BenefitSection.jsx";
+import Footer from "../../components/Footer.jsx";
+
+
+const Home = () => {
+    return(
+        <div className=" pt-27">
+            <HeroSection/>
+            <TestimonialSection/>
+            <BenefitSection/>
+            <Footer/>
+        </div>
+    )
+}
+
+
+export default Home
