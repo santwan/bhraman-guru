5cc07391228028133a5573e8016db354d8495053
santwan <santwanpathak1@gmail.com>
2025-04-21 00:28:21 +0530

    Update Navbar.jsx

--- frontend/src/components/global/Navbar.jsx
+++ frontend/src/components/global/Navbar.jsx
@@ -93,7 +93,7 @@ const Navbar = () => {
                   <UserButton.Link
                     label="Trip History"
                     labelIcon={<svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 24 24" fill="currentColor"><path d="M19 3H5a2 2 0 0 0-2 2v14a2..."/></svg>}
-                    onClick={() => navigate("/trip-history")}
+                    href="/trip-history"
                   />
                   <UserButton.Action label="manageAccount" />
                   <UserButton.Action label="signOut" />
