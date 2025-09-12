f2b30ef9d1712b6514dccbe077654e7cafab151f
santwanpathak1 <santwanpathak1@gmail.com>
2025-09-12 14:15:32 +0000

    fix: backend route for places

--- backend/src/routes/v1/places.routes.js
+++ backend/src/routes/v1/places.routes.js
@@ -4,9 +4,9 @@ import { asyncHandler } from "../../utils/asyncHandler.js";
 
 const router = Router();
 
-// POST /api/v1/places/details
-router.post("/places/place-details", asyncHandler(getPlaceImage));
+// POST /api/v1/places/place-details
+router.post("/place-details", asyncHandler(getPlaceImage));
 
-router.post("/places/hotel-details", asyncHandler(getHotelImage));
+router.post("/hotel-details", asyncHandler(getHotelImage));
 
 export default router;
